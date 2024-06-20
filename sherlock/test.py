8702a379be6c557d2d6f454e35b4bf4483571378
BoltR6 <johnwallacewhite@gmail.com>
2024-06-19 20:21:26 -0500

    chore: Removed unneeded test file

--- sherlock/test.py
+++
@@ -1,8 +0,0 @@
-
-def main():
-    service_username = "kFG7kh2U6KWzVQLayuyZU1P5"
-    service_password = "wJB68Pf1VK75Sxo2As1mPXfx"
-    host = "atlanta.us.socks.nordhold.net"
-    port = "1080"
-    
-main()
